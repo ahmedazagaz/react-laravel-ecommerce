f71df6d6507c60246a94efdd9faf179897afc1ac
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-21 20:49:21 +0100

    Add resource routes for brands in web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+// Brand Routes
 Route::resource('brands', \App\Http\Controllers\BrandController::class);
 
 require __DIR__.'/settings.php';
