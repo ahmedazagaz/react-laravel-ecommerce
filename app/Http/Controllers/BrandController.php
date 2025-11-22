48cdc7aa844e1f63bad79b246703714df3e6e79f
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-22 19:12:36 +0100

    update controller brand

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    public function index(){
+        return response()->json(Brand::all());
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name'  => 'required|string|max:255',
+            'slug'  => 'required|string|unique:brands,slug',
+            'image' => 'nullable|string',
+            'is_active' => 'boolean',
+        ]);
+
+        $brand = Brand::create($validated);
+
+        return response()->json($brand, 201);
+    }
 }
