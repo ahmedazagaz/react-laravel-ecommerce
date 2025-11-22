ef9f560d53442e5b35c6007fd156f2e5e672c880
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-22 19:22:40 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BrandController;
 
+// Brand API Routes
 Route::apiResource('brands', BrandController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
