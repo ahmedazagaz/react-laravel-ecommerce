eabf8c710e5dbad48947f038ae44dbfa04cce263
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-27 22:41:54 +0100

    Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'is_stock',
+        'is_sale',
+    ];
+    
 }
