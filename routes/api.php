5283f93e6d2479f5a26c921ddbe95a7097be886f
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-27 22:47:17 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductController;
 
 // Brand API Routes
 Route::apiResource('brands', BrandController::class);
+
+// Product API Routes
+Route::apiResource('products', ProductController::class);
